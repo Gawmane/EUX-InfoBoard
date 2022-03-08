329771f0ea24dc4a3f7f30e5236923a9bcfe511b
MagnusVinther <magn790m@elev.techcollege.dk>
2022-03-08 12:38:09 +0100

    Ur og dato funktion uden styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // Download selv axios, sass, react
 
 import './App.scss';
+import { ClockFunc } from './Components/Partials/Clock/Clock';
 
 function App() {
   return (
     <div>
       <h1>hej igen</h1>
+      <ClockFunc />
     </div>
   );
 }
