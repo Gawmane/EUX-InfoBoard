77c70ea3f1d44a7804581e04cbdc719bc79c8212
Eva-Renata <all2evi@yahoo.com>
2022-03-08 12:57:26 +0100

    ugens menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // Download selv axios, sass, react
 
 import './App.scss';
+import { Menu } from './Components/Partials/FoodPlan/FoodPlan';
 
 function App() {
   return (
     <div>
       <h1>hej igen</h1>
+      <Menu />
     </div>
   );
 }
