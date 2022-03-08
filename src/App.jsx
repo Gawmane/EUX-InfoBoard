aa15a7da4dc93daa8fdabbc86f01f720fe628ab8
Gawmane <gawman.e@hotmail.com>
2022-03-08 12:49:25 +0100

    newfeed funktion minus style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // Download selv axios, sass, react
 
 import './App.scss';
+import {  NewsFeed } from './Components/Partials/NewsFeed/NewsFeed';
 
 function App() {
   return (
     <div>
       <h1>hej igen</h1>
+      <NewsFeed/>
     </div>
   );
 }
