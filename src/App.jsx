03f62b5d18fe440e0e88e2239974d960f416a5b9
MagnusVinther <magn790m@elev.techcollege.dk>
2022-03-11 09:20:54 +0100

    Korrektur

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,20 @@ import './App.scss';
 import {  NewsFeed } from './Components/Partials/NewsFeed/NewsFeed';
 import { ClockFunc } from './Components/Partials/Clock/Clock';
 import { Menu } from './Components/Partials/FoodPlan/FoodPlan';
-
 import { ShowWeather } from './Components/Partials/WeatherBox/WeatherBox';
 import { Activites } from './Components/Partials/Activities/Activites';
-
 import { BusPlan } from './Components/Partials/BusPlan/BusPlan';
 
 
 function App() {
   return (
     <div>
-
       <Activites/>
-        <ShowWeather/> 
-     
-       <NewsFeed/> 
-       <Menu /> 
-       <ClockFunc /> 
-
+      <ShowWeather/> 
+      <NewsFeed/> 
+      <Menu /> 
+      <ClockFunc />
       <BusPlan />
-
     </div>
   );
 }
