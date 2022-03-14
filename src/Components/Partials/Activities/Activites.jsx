b755becaad70d512c47312742e3f74a0113d1413
Eva-Renata <all2evi@yahoo.com>
2022-03-14 08:38:04 +0100

    sortering aktiviteter

--- src/Components/Partials/Activities/Activites.jsx
+++ src/Components/Partials/Activities/Activites.jsx
@@ -4,38 +4,48 @@ import styles from './Activities.module.scss'
 //1st way
 import Data from "./Activities.json";
 
-
 export const Activites = () => {
   
   const [activities, setActivities] = useState([]);
   
     useEffect(()=>{
+        let activitiesArray = Data.value;
+        console.log(activitiesArray);
+        //..................... sort all activites by StartDate ascending
+        activitiesArray.sort((a,b)=> {
+          return new Date(a.StartDate) - new Date(b.StartDate);
+        })
+        const today = new Date();
+        //...................set date manually for testing
+        // today.setDate(9)
+        // today.setHours(6,0)
+        //.......................... //filter all past activities 
+        // activitiesArray = activitiesArray.filter((activity) => {
+        //   return new Date(activity.StartDate) > today;
+        // })
 
-        //Activities imported from json
-        setActivities(Data.value)
+        //Activities imported from jsons
+        setActivities(activitiesArray)
         // console.log(Data.value);
         
         //Dependency array [] - render 1 gang og cleaner så 
       }, []);
       
       // Returner en tabel med en række(tr) med overskrifter(th)
-
-      
       return (
         <>
       <table className={styles.table}>
         <tbody>
-      <tr>
-    <th>Kl.</th>
-    <th>Fag</th>
-    <th>Udannelse</th>
-    <th>Hold</th>
-    <th>Lokale</th>
+          <tr>
+            <th>Kl.</th>
+            <th>Fag</th>
+            <th>Udannelse</th>
+            <th>Hold</th>
+            <th>Lokale</th>
+          </tr>
 
-  </tr>
-  {/* Laver map hvor vi henter vores aktivites data */}
-      {activities && activities.map((item, index) => {
-        
+        {/* Laver map hvor vi henter vores aktivites data */}
+        {activities && activities.map((item, index) => {
         item.StartDate = item.StartDate.replace("+01:00", "+00:00");
             // Sætter tidsformat til time:minut på property item.Time
         item.Time = new Date(item.StartDate).toLocaleTimeString(
@@ -43,12 +53,8 @@ export const Activites = () => {
           hour: '2-digit',
           minute: '2-digit'
         })
-        
-        
         //   Returner en række med vores kl,fag,uddanelse,holdnummer og lokale
-        
         return(
-          
           <tr key={index} >
               <td>{item.Time}</td>
                <td>{item.Subject ? item.Subject : "?"}</td>
@@ -57,10 +63,7 @@ export const Activites = () => {
                <td>{item.Room ? item.Room : "I værksted"}</td>
             </tr> 
           )
-          
-          
         })}
-        
           </tbody>
         </table>
      </>   
