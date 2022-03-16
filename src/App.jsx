965d317f2c67e82d67ccf846806386e9343009a9
Eva-Renata <all2evi@yahoo.com>
2022-03-16 13:00:24 +0100

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 // Download selv axios, sass, react, "npm install react-icons --save"
 
-import style from './App.module.scss';
-import {  NewsFeed } from './Components/Partials/NewsFeed/NewsFeed';
-import { ClockFunc } from './Components/Partials/Clock/Clock';
-import { Menu } from './Components/Partials/FoodPlan/FoodPlan';
-import { ShowWeather } from './Components/Partials/WeatherBox/WeatherBox';
-import { Activites } from './Components/Partials/Activities/Activites';
-import { BusPlan } from './Components/Partials/BusPlan/BusPlan';
-import logo from '../src/Assets/Images/EUX-logo.png';
+import style from "./App.module.scss";
+import { NewsFeed } from "./Components/Partials/NewsFeed/NewsFeed";
+import { ClockFunc } from "./Components/Partials/Clock/Clock";
+import { Menu } from "./Components/Partials/FoodPlan/FoodPlan";
+import { ShowWeather } from "./Components/Partials/WeatherBox/WeatherBox";
+import { Activites } from "./Components/Partials/Activities/Activites";
+import { BusPlan } from "./Components/Partials/BusPlan/BusPlan";
+import logo from "../src/Assets/Images/EUX-logo.png";
 
 function App() {
   return (
     <>
-    <div className={style.wrapper}>
-      <section className={style.leftside}>
-      <img src={logo} alt="Logo" className={style.logo}/>
-      <ClockFunc />
-      <ShowWeather/> 
-      <Menu />
-      <BusPlan />
-      </section>
-      
-      <section className={style.aktiviteter}>
-       <Activites/>
-      </section>
-    </div>
-    <NewsFeed/>
+      <div className={style.wrapper}>
+        <section className={style.leftside}>
+          <img src={logo} alt="Logo" className={style.logo} />
+          <ClockFunc />
+          <ShowWeather />
+          <Menu />
+          <BusPlan />
+        </section>
+
+        <section className={style.aktiviteter}>
+          <Activites />
+        </section>
+      </div>
+      <NewsFeed />
     </>
-    
-   
   );
 }
 
--- src/Components/Partials/Clock/Clock.jsx
+++ src/Components/Partials/Clock/Clock.jsx
@@ -1,40 +1,39 @@
-import React, { useState, useEffect } from "react"
-import Style from "./Clock.module.scss"
+import React, { useState, useEffect } from "react";
+import Style from "./Clock.module.scss";
 
 // Laver en funktion til uret
 export const ClockFunc = () => {
-    // Laver en dateState med et useState hook
-    const [ DateState, setDateState ] = useState(new Date())
-    // Tilføjer useEffect således at man kan sætte et interval og så uret opdaterer sig selv.
-    useEffect (() => {
-        // Opdaterer nu uret hvert 30 sekund.
-        setInterval(() => setDateState(new Date()), 30000);
-    }, []);
+  // Laver en dateState med et useState hook
+  const [DateState, setDateState] = useState(new Date());
+  // Tilføjer useEffect således at man kan sætte et interval og så uret opdaterer sig selv.
+  useEffect(() => {
+    // Opdaterer nu uret hvert 30 sekund.
+    setInterval(() => setDateState(new Date()), 1000);
+  }, []);
 
-    // Her returneres ClockFunc
-    return (
-        // Tilføjer class og styling dertil på div.
-        <div className={Style.clock}>
-            
-            <p>
-                {'  '}
-                {/* Tilføjer dato med dag, måned og år, og fortæller hvordan de skrives.
+  // Her returneres ClockFunc
+  return (
+    // Tilføjer class og styling dertil på div.
+    <div className={Style.clock}>
+      <p>
+        {"  "}
+        {/* Tilføjer dato med dag, måned og år, og fortæller hvordan de skrives.
                 da-DK gør så månederne vil blive skrevet på dansk */}
-                {DateState.toLocaleDateString('da-DK', {
-                    day: 'numeric', // dag skrives i tal.
-                    month: 'long', // long skrive måneden i dens helhed.
-                    year: 'numeric' // år skrives i tal.
-                })}
-            </p>
-            <p className={Style.timeInd}>
-                {/* Bruger toLocaleString metoden for at kunne ændre på tidspunkt. 
+        {DateState.toLocaleDateString("da-DK", {
+          day: "numeric", // dag skrives i tal.
+          month: "long", // long skrive måneden i dens helhed.
+          year: "numeric", // år skrives i tal.
+        })}
+      </p>
+      <p className={Style.timeInd}>
+        {/* Bruger toLocaleString metoden for at kunne ændre på tidspunkt. 
                 Uret vises som et engelsk ur, da det er et 24-timers ur. */}
-                {DateState.toLocaleString('en-GB', {
-                    // Tilføjer time og minut til klokken, og fortæller de har numerisk værdi.
-                    hour: 'numeric', // Timer skrives i tal.
-                    minute: 'numeric' // Minutter skrives i tal.
-                })}
-            </p>
-        </div>
-    )
-}
+        {DateState.toLocaleString("en-GB", {
+          // Tilføjer time og minut til klokken, og fortæller de har numerisk værdi.
+          hour: "numeric", // Timer skrives i tal.
+          minute: "numeric", // Minutter skrives i tal.
+        })}
+      </p>
+    </div>
+  );
+};
